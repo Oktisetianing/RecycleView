5c62359ce3176713d26343d88a5f91236127a277
Oktisetianing <oktisetianing94@gmail.com>
2023-06-30 02:20:04 +0700

    edit tulisan

--- app/src/main/java/com/example/tugasrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/tugasrecyclerview/MainActivity.java
@@ -18,9 +18,7 @@ public class MainActivity extends AppCompatActivity {
         WisataModel [] datawisata = new WisataModel[]{
                 new WisataModel(R.drawable.candiprambanan,"Candi Prambanan", "Buka pukul 06.30 WIB", "Candi Prambanan adalah kompleks candi Hindu terbesar di Indonesia yang dibangun pada abad ke-9 masehi. Candi ini dipersembahkan untuk Trimurti, tiga dewa utama Hindu yaitu Brahma sebagai dewa pencipta, Wishnu sebagai dewa pemelihara, dan Siwa sebagai dewa pemusnah.", "Jl. Raya Solo - Yogyakarta No. 16"),
                 new WisataModel(R.drawable.bhumimerapi,"Bhumi Merapi", "Buka pukul 09.00 WIB", "Agrowisata ini menawarkan konsep pertanian, perkebunan, dan peternakan, sehingga para pengunjung juga bisa mendapatkan banyak ilmu.", "Hargobinangun, Pakem, Sleman"),
-                new WisataModel(R.drawable.waterboomjogja,"Waterboom Jogja", "Buka pukul 09.00 WIB", "Waterboom Jogja adalah alah satu waterpark terbesar di asia tenggara yang berlokasi di kota wisata terbesar kedua di Indonesia yaitu Yogyakarta.\\n\" +\n" +
-                        "                        \"\\n\" +\n" +
-                        "                        \"Waterboom Jogja telah tersertifikasi CHSE (Clean, Health, Safety, & Enviroment) dengan score 100% dari Kemenparekraf RI dan Sucofindo.", "Jenengan, Maguwoharjo, Sleman"),
+                new WisataModel(R.drawable.waterboomjogja,"Waterboom Jogja", "Buka pukul 09.00 WIB", "Waterboom Jogja adalah alah satu waterpark terbesar di asia tenggara yang berlokasi di kota wisata terbesar kedua di Indonesia yaitu Yogyakarta.Waterboom Jogja telah tersertifikasi CHSE (Clean, Health, Safety, & Enviroment) dengan score 100% dari Kemenparekraf RI dan Sucofindo.", "Jenengan, Maguwoharjo, Sleman"),
                 new WisataModel(R.drawable.museumullen,"Museum Ullen", "Buka pukul 08.30 WIB", "Museum Ullen Sentalu adalah tempat wisata di Sleman, yang menyajikan nilai sejarah yang edukatif, dalam balutan keseruan wisata yang cocok untuk semua usia.", "Hargobinangun, Pakem, Sleman"),
         };
 
